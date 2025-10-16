74f94977eff9a2513db20f89f28cf30f77a01479
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-10-16 16:28:04 +0300

    Add validation schemas

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -23,7 +23,7 @@ export const createNote = async (req, res) => {
 
 export const deleteNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndDelete({ _id: noteId });
+  const note = await Note.findOneAndDelete({ _id: noteId });
   if (!note) {
     next(createHttpError(404, 'Note not found'));
     return;
@@ -33,7 +33,7 @@ export const deleteNote = async (req, res, next) => {
 
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate({ _id: noteId }, req.body, { new: true });
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, { new: true });
   if (!note) {
     next(createHttpError(404, 'Note not found'));
     return;
