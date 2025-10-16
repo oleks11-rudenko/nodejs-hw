e4d0388946299aa010becc50b913653e72862319
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-10-16 18:39:59 +0300

    Add filtering and pagination

--- src/models/note.js
+++ src/models/note.js
@@ -33,4 +33,6 @@ const noteSchema = new Schema(
   }
 );
 
+noteSchema.index({ title: 'text', content: 'text' });
+
 export const Note = model('Note', noteSchema);
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -11,7 +11,7 @@ export const getAllNotesSchema = {
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
-    search: Joi.string(),
+    search: Joi.string().allow(''),
   }),
 };
 
@@ -22,7 +22,7 @@ export const noteIdSchema = {
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
-    content: Joi.string(),
+    content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }),
 };
@@ -31,7 +31,7 @@ export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({ noteId: Joi.string().custom(objectIdValidator).required() }),
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
-    content: Joi.string(),
+    content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }),
 };
