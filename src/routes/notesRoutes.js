7cf95a483a874a52fead0014a3622e4d393869db
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-10-17 13:05:29 +0300

    Make some changes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import {
-  getNotes,
+  getAllNotes,
   getNoteById,
   createNote,
   deleteNote,
@@ -16,7 +16,7 @@ import {
 
 const router = Router();
 
-router.get('/notes', celebrate(getAllNotesSchema), getNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', celebrate(createNoteSchema), createNote);
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3030;
 
 app.use(logger);
 app.use(express.json());
