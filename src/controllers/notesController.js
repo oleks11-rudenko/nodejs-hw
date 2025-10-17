678e1256f0564fedf9a7cd62cab524d3131c2874
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-10-17 13:03:45 +0300

    Make some changes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
@@ -23,7 +23,7 @@ export const createNote = async (req, res) => {
 
 export const deleteNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndDelete({ _id: noteId });
+  const note = await Note.findOneAndDelete({ _id: noteId });
   if (!note) {
     next(createHttpError(404, 'Note not found'));
     return;
@@ -33,7 +33,7 @@ export const deleteNote = async (req, res, next) => {
 
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate({ _id: noteId }, req.body, { new: true });
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, { new: true });
   if (!note) {
     next(createHttpError(404, 'Note not found'));
     return;
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3030;
 
 app.use(logger);
 app.use(express.json());
