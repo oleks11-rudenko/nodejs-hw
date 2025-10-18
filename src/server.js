4ea9e8827df267d7354b3ba9484df3ded7000332
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-10-18 15:54:33 +0300

    Made first part

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
@@ -16,8 +18,10 @@ const PORT = process.env.PORT ?? 3030;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(helmet());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
