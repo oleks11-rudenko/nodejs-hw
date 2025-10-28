1f3862b0c8345c04aebbd8185892d3f707b4d2ce
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-10-28 18:29:06 +0200

    Solved

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -23,6 +24,7 @@ app.use(helmet());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
